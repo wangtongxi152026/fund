439f88175da029bd9edd47ccfa693dac42a2818d
wangtongxi <1520268401@qq.com>
2021-11-17 16:00:48 +0800

    添加json写入

--- FundAcitonTest.py
+++ FundAcitonTest.py
@@ -4,23 +4,20 @@ import json
 import pandas as pd
 import numpy as np
 
-def resolve():
-    file = open('./store.json', "rb")
-    fileJson = json.load(file)[5]
-    code = fileJson["code"]
-    portion = fileJson["portion"]
-    start_cach = fileJson["start_cach"]
-    return {'code': code, 'portion': portion, 'start_cach': start_cach}
-
+jsonIdx = 0
+jsonData = []
+fileJson = {}
+with open('./store.json', "rb") as file:
+    jsonData = json.load(file)
+    fileJson = jsonData[jsonIdx]
 
 # code = '005609'
 # portion = 399.34
 # start_cach = 177.84
-# start = '2021-07-01'
-code = resolve()['code']
-portion = resolve()['portion']
-start_cach = resolve()['start_cach']
 
+code = fileJson['code']
+portion = fileJson['portion']
+start_cach = fileJson['start_cach']
 start = '2021-07-01'
 current_time = time.strftime('%Y-%m-%d', time.localtime())
 value = [[] for a in range(2)]
@@ -68,7 +65,7 @@ total_latest_4_sum = (data.loc[data.index[0], 0]
                       + data.loc[data.index[2], 0]
                       + data.loc[data.index[3], 0])
 print(data.loc[data.index[0], 0])
-print(data)
+# print(data)
 res1 = requests.get(
     'http://fundgz.1234567.com.cn/js/{code}.js?'.format(code=code))
 datasplite = json.loads(res1.text[8:-2])
@@ -76,6 +73,9 @@ print(res1.text[8:-2])
 
 # 估算净值
 current_value = float(datasplite['gsz'])
+jsonData[jsonIdx]['gsz'] = current_value
+jsonData[jsonIdx]['name'] = datasplite['name']
+
 latest_ma5 = round((total_latest_4_sum+current_value)/5, 4)
 lastset_trend = round((latest_ma5/data.loc[data.index[0], 'ma5']-1), 4)
 if lastset_trend*data.loc[data.index[0], 'trend'] > 0:
@@ -89,7 +89,7 @@ value[0] = total_value[0]['jjjz']
 date2 = total_value[1]['fbrq'].split(' ')[0]
 value[1] = total_value[1]['jjjz']
 cash = portion*float(value[1])
-print('cash',cash)
+print('cash', cash)
 change = (current_value-float(value[0]))/float(value[0])*100
 print('单日情况:', current_time, '估算净值:', current_value, 'Change:', change, '%')
 print('趋势维持天数:', index, '最新5日均值:', latest_ma5, '最新趋势:', lastset_trend)
@@ -98,6 +98,7 @@ print(date1, value[0])
 print(date2, value[1])
 
 for idx, i in enumerate(value):
+    time = [date1, date2][idx].split(' ')[0]
     buy_factor = 5.5
     sell_factor = 5
     if index > 11 and lastset_trend < 0:
@@ -126,14 +127,22 @@ for idx, i in enumerate(value):
         buy_factor = buy_factor/1.25
         sell_factor = sell_factor/1.25
     float_value = float(i)
-
-    if current_value > float_value: 
+    if current_value > float_value:
         reaction = -(current_value/float_value-1)*sell_factor*portion
         factor = sell_factor
-        print('相较于{time}的操作:卖出份额'.format(
-            time=[date1, date2][idx].split(' ')[0]),  reaction, "系数:", factor)
+
     else:
         reaction = (float_value/current_value-1)*buy_factor*cash
         factor = buy_factor
-        print('相较于{time}的操作:买入金额'.format(
-            time=[date1, date2][idx].split(' ')[0]),  reaction, "系数:", factor)
+
+    action = '买入金额' if(reaction > 0) else '卖出份额'
+    print('相较{time}的操作:{action}'.format(
+        time=time, action=action),  reaction, "系数:", factor)
+    if idx == 0:
+        jsonData[jsonIdx]['action'] = reaction
+        if 'sell_value' in jsonData[jsonIdx].keys():
+            del jsonData[jsonIdx]['sell_value']
+        if reaction < 0:
+            jsonData[jsonIdx]['sell_value'] = reaction*current_value
+with open("./store.json", "w",encoding='utf-8') as dump_f:
+    json.dump(jsonData, dump_f,ensure_ascii=False)
